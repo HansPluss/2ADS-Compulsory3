0daaf6b3cc3fd7ddb6c2292ebb695919277075d6
Hans <111863567+HansPluss@users.noreply.github.com>
2023-10-29 14:33:54 +0100

    Changed name
    + Removed old code

---
+++ TreeAndGraphLib/GeneralTreeLib/GraphLib.cpp
@@ -0,0 +1,144 @@
+#include "GraphLib.h"
+
+GraphLib::GraphLib(int vertices)
+{
+	this->numOfVertices = vertices;
+	adjLists = new list<int>[vertices];
+	std::srand(static_cast<unsigned>(std::time(0))); // random seed
+}
+GraphLib::~GraphLib() {
+	
+	//To prevent memeory leak
+	for (int v = 0; v < numOfVertices; ++v) {
+		adjLists[v].clear();
+	}
+	delete[] adjLists;
+}
+void GraphLib::InsertEdge(int v, int e)
+{
+	adjLists[v].push_back(e);
+	adjLists[e].push_back(v);
+	//adjLists[v].push_back(e); old code
+}
+
+list<int> GraphLib::GetAdjacentNodes(int vertex) const
+{
+	if (vertex >= 0 && vertex < numOfVertices) {
+		return adjLists[vertex];
+
+	}
+	else {
+		//error message
+		return {};
+	}
+}
+void GraphLib::InsertVertexAtRandom() {
+	int randomPosition = std::rand() % (numOfVertices + 1);
+	InsertVertexAt(std::rand(), randomPosition);
+}
+void GraphLib::InsertVertexAt(int data, int position)
+{
+	if (position >= 0 && position <= numOfVertices) {
+		
+		adjLists[position].push_back(data);
+
+	
+		for (int i = 0; i < numOfVertices; ++i) {
+			if (i != position) {
+				adjLists[i].push_back(data);
+				adjLists[position].push_back(i);
+			}
+		}
+	}
+	numOfVertices++;
+
+}
+
+void GraphLib::PrintGraph()
+{
+	for (int v = 0; v < numOfVertices; ++v) {
+		std::cout << "Adjacency list of vertex " << v << ": ";
+		for (const auto& neighbor : adjLists[v]) {
+			std::cout << neighbor << " ";
+		}
+		std::cout << std::endl;
+	}
+}
+
+list<int> GraphLib::GetVertices()
+{
+	std::list<int> vertices;
+
+	for (int v = 0; v < numOfVertices; ++v) {
+		vertices.push_back(v);
+	}
+
+	return vertices;
+}
+
+void GraphLib::DeleteEdge(int v, int e)
+{
+	auto it = std::find(adjLists[v].begin(), adjLists[v].end(), e);
+
+	if (it != adjLists[v].end()) {
+		adjLists[v].erase(it);
+	}
+}
+
+void GraphLib::DFS(int startvertex)
+{
+	//get starting vertex then mark it as "visited"
+	list<int> visited(numOfVertices, false);
+
+	cout << startvertex << " ";
+	DFSRecursiveFunc(startvertex, visited);
+	cout << endl;
+
+	/*list<int>::iterator it;
+	for (it = adjLists[startvertex].begin(); it != adjLists[startvertex].end(); it++) {
+		if (!visited[*it]) {
+			DFS(*it);
+		}
+
+	}*/
+}
+
+void GraphLib::DFSRecursiveFunc(int vertex, list<int>& visited)
+{
+
+	visited.push_back(vertex);
+	std::cout << vertex << " ";
+
+	for (const auto& neighbor : adjLists[vertex]) {
+		auto it = std::find(visited.begin(), visited.end(), neighbor);
+		if (it == visited.end()) {
+			DFSRecursiveFunc(neighbor, visited);
+		}
+	}
+}
+
+int GraphLib::Size() const
+{
+	return numOfVertices;
+}
+
+bool GraphLib::isEmpty() const
+{
+	return numOfVertices == 0;
+}
+//int main() {
+//
+//	GraphLib graph(8);
+//	graph.InsertEdge(0, 1);
+//	graph.InsertEdge(0, 2);
+//	graph.InsertEdge(1, 3);
+//	graph.InsertEdge(2, 3);
+//	graph.InsertEdge(3, 4);
+//	graph.InsertEdge(3, 5);
+//	graph.InsertEdge(4, 6);
+//	graph.InsertEdge(5, 6);
+//	graph.DFS(1);
+//	
+//}
+
+
---
+++ TreeAndGraphLib/GeneralTreeLib/TreeAndGraph.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <list>
+#include "GeneralTree.h"
+#include "GraphLib.h"
+using namespace std;
+
+int main() {
+    GeneralTree tree;
+    GeneralTree::Node* root = tree.CreateNode(1, nullptr);
+    GeneralTree::Node* parent = tree.CreateNode(4, root);
+    GeneralTree::Node* parent1 = tree.CreateNode(7, root);
+    GeneralTree::Node* child2 = tree.CreateNode(8, parent1);
+    GeneralTree::Node* child3 = tree.CreateNode(9, child2);
+    
+     tree.PrintTree(root);
+    tree.Preorder(root);
+    
+     //Delete a node (example: child2)
+     tree.DeleteNode(child2);
+     tree.Preorder(root);
+
+     GraphLib graph(8);
+     	graph.InsertEdge(0, 1);
+     	graph.InsertEdge(0, 2);
+     	graph.InsertEdge(1, 3);
+     	graph.InsertEdge(2, 3);
+     	graph.InsertEdge(3, 4);
+     	graph.InsertEdge(3, 5);
+     	graph.InsertEdge(4, 6);
+     	graph.InsertEdge(5, 6);
+     	//graph.DFS(1);
+
+    return 0;
+}
+
+
+
